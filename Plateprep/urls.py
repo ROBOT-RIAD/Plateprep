4084a07c4d4e786d5c777b070cafa4edd8086b4c
MD RIAD HOSSEN <riadhosain420@gmail.com>
2025-06-16 11:16:16 +0600

    update code

--- Plateprep/urls.py
+++ Plateprep/urls.py
@@ -25,7 +25,7 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="My API",
+      title="Nobutts",
       default_version='v1',
       description="API documentation for my project",
    ),
